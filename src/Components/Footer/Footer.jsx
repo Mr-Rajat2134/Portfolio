6a2b6f7f13aa7d5392f9d0be95acea6faa98f492
Rajat jain <rajatjain2134@gmail.com>
2024-03-13 17:49:16 +0530

    updates resume section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,61 +1,16 @@
-// import React from 'react'
-// import './Footer.css'
+import React from 'react'
+import './Footer.css'
 
-// const Footer = () => {
-//   return (
-//     <section className='footer'>
-
-// Copyright &#169; 2024 RajatPortfolio. All right received. 
-
-
-
-//     </section>
-//   )
-// }
-
-// export default Footer
-import React, { useState } from 'react';
-import { Button, Container, TextField, Typography } from '@mui/material';
-
-function ImageUpload() {
-  const [image, setImage] = useState(null);
-
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        setImage(reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
+const Footer = () => {
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" gutterBottom>
-        Image Upload
-      </Typography>
-      <input
-        accept="image/*"
-        id="contained-button-file"
-        type="file"
-        style={{ display: 'none' }}
-        onChange={handleImageChange}
-      />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" component="span">
-          Upload Image
-        </Button>
-      </label>
-      {image && (
-        <div style={{ marginTop: 20 }}>
-          <Typography variant="h6">Preview:</Typography>
-          <img src={image} alt="Uploaded" style={{ maxWidth: '100%', marginTop: 10 }} />
-        </div>
-      )}
-    </Container>
-  );
+    <section className='footer'>
+
+Copyright &#169; 2024 RajatPortfolio. All right received. 
+
+
+
+    </section>
+  )
 }
 
-export default ImageUpload;
+export default Footer
